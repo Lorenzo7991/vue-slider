70ba74a8041310f9203c50094077779bb18b173f
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-22 16:38:02 +0100

    add dynamic gallery, dynamic thumbnails, prev and next butns logic

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+console.log('VUE-OK', Vue);
+
+const { createApp } = Vue;
+const app = createApp({
+    data() {
+        return {
+            destinations: destinations,
+            currentIndex: 0,
+        };
+    },
+    methods: {
+        direction(direction) {
+            if (direction === 'next') {
+                this.currentIndex += 1;
+                if (this.currentIndex === this.destinations.length) {
+                    this.currentIndex = 0;
+                }
+            } else if (direction === 'prev') {
+                this.currentIndex -= 1;
+                if (this.currentIndex < 0) {
+                    this.currentIndex = this.destinations.length - 1;
+                }
+            }
+        },
+    },
+});
+
+app.mount('#root');
